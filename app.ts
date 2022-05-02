a5456cee976f95f30f9480b173acecf30bc1e9ce
Rafael Castillo <pcastill@mac.com>
2022-05-02 14:38:41 -0500

    typeof

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 function add(n1: number, n2: number) {
   console.log(typeof n1)
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number' ) {
+  //   throw new Error('Wrong input')
+  // }
   return n1 + n2;
 }
 
